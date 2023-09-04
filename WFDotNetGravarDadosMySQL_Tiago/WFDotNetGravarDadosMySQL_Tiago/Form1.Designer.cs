cf38b0ed7a026833b95f58b2c8a02eb4440a210a
Roberval Santana <robervalssprogramacao@gmail.com>
2023-09-04 16:47:48 -0300

    Commit Inicial c#

---
+++ WFDotNetGravarDadosMySQL_Tiago/WFDotNetGravarDadosMySQL_Tiago/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WFDotNetGravarDadosMySQL_Tiago
+{
+    partial class FrmCarro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstCarro = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtBusca = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descrição do Carro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Placa do Carro";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(12, 59);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(270, 20);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(12, 116);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(270, 20);
+            this.txtPlaca.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Gravar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstCarro
+            // 
+            this.lstCarro.ContextMenuStrip = this.contextMenuStrip1;
+            this.lstCarro.HideSelection = false;
+            this.lstCarro.Location = new System.Drawing.Point(288, 59);
+            this.lstCarro.MultiSelect = false;
+            this.lstCarro.Name = "lstCarro";
+            this.lstCarro.Size = new System.Drawing.Size(360, 226);
+            this.lstCarro.TabIndex = 7;
+            this.lstCarro.UseCompatibleStateImageBehavior = false;
+            this.lstCarro.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lstCarro_ItemSelectionChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(142, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(141, 22);
+            this.toolStripMenuItem1.Text = "Excluir Carro";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // txtBusca
+            // 
+            this.txtBusca.Location = new System.Drawing.Point(288, 33);
+            this.txtBusca.Name = "txtBusca";
+            this.txtBusca.Size = new System.Drawing.Size(279, 20);
+            this.txtBusca.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(288, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Buscar Carro";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(573, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(12, 161);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 11;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(111, 161);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Excluir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // FrmCarro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 297);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBusca);
+            this.Controls.Add(this.lstCarro);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCarro";
+            this.Text = " ";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lstCarro;
+        private System.Windows.Forms.TextBox txtBusca;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WFDotNetGravarDadosMySQL_Tiago/WFDotNetGravarDadosMySQL_Tiago/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace WFDotNetGravarDadosMySQL_Tiago
+{
+    public partial class FrmCarro : Form
+    {
+        
+
+        private MySqlConnection conexao;
+        private string data_source = "datasource=localhost;username=root;password=admin;database=transporte";
+        private int ?id_Carro_Selecionado = null;
+
+        public FrmCarro()
+        {
+            InitializeComponent();
+            lstCarro.View = View.Details;
+            lstCarro.LabelEdit = true;
+            lstCarro.AllowColumnReorder = true;
+            lstCarro.FullRowSelect = true;
+            lstCarro.GridLines = true;
+            
+            lstCarro.Columns.Add("Id", 50, HorizontalAlignment.Left);
+            lstCarro.Columns.Add("Descrição", 150, HorizontalAlignment.Left);
+            lstCarro.Columns.Add("Placa", 150, HorizontalAlignment.Left);
+            carrega_Carro();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //criar Conexao  com Mysql
+                conexao = new MySqlConnection(data_source);
+
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = conexao;
+
+
+                if (id_Carro_Selecionado == null)
+                {
+                    cmd.CommandText = "Insert into Carro( carr_tx_descricao, carr_tx_placa) VALUES " +
+                        " ( @descricao, @placa)";
+
+                    cmd.Parameters.AddWithValue("@descricao", txtDescricao.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@placa", txtPlaca.Text.ToUpper());
+
+                    cmd.Prepare();
+                     
+                    cmd.ExecuteNonQuery();
+
+                    cmd.Parameters.Clear();
+                    /*   string sql = "Insert into Carro( carr_tx_descricao, carr_tx_placa) VALUES " +
+                           " ('" + txtDescricao.Text.ToUpper() + "', '" + txtPlaca.Text.ToUpper() + "')";
+
+                        MySqlCommand comando = new MySqlCommand(sql, conexao);
+                       conexao.Open();
+                       comando.ExecuteReader(); */
+                    MessageBox.Show("CARRO cadastrado com sucesso!",
+                        "Inclusão", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else 
+                {
+
+                    cmd.CommandText = "UPDATE Carro SET  carr_tx_descricao = @descricao, carr_tx_placa = @placa " +
+                        "where carr_id_carro = @id";
+
+                    cmd.Parameters.AddWithValue("@descricao", txtDescricao.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@placa", txtPlaca.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@id", id_Carro_Selecionado);
+                    cmd.Prepare();
+
+                    cmd.ExecuteNonQuery();
+
+                    cmd.Parameters.Clear();
+
+                    MessageBox.Show("CARRO Alterado com sucesso!",
+                        "Alteração", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                limpa_formulario();
+                carrega_Carro();
+
+            }
+            catch (MySqlException Ex)
+            {
+                MessageBox.Show("Erro" + Ex.Number + "Ocorreu : " + Ex.Message, "Erro", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+            finally {
+                conexao.Close();
+            }
+        }
+
+        private void carrega_Carro()
+        {
+            try
+            {
+                /*   --------------------------------------------- */
+                //criar Conexao  com Mysql
+                conexao = new MySqlConnection(data_source);
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = conexao;
+
+                cmd.CommandText = "Select * FROM Carro ORDER BY carr_tx_descricao DESC";
+                cmd.Prepare();
+
+                MySqlDataReader listaCarroSelect = cmd.ExecuteReader();
+
+                lstCarro.Items.Clear();
+
+                while (listaCarroSelect.Read())
+                {
+                    string[] linhacarro = {
+                    listaCarroSelect.GetString(0),
+                    listaCarroSelect.GetString(1),
+                    listaCarroSelect.GetString(2),
+                    };
+                    var linha_listview = new ListViewItem(linhacarro);
+
+                    lstCarro.Items.Add(linha_listview);
+                }
+
+
+            }
+            catch (MySqlException Ex)
+            {
+                MessageBox.Show("Erro" + Ex.Number + "Ocorreu : " + Ex.Message, "Erro", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*   --------------------------------------------- */
+                //criar Conexao  com Mysql
+                conexao = new MySqlConnection(data_source);
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                cmd.Connection = conexao;
+
+                cmd.CommandText =  "Select * FROM Carro WHERE " +
+                            " carr_tx_descricao LIKE @strBusca OR carr_tx_placa LIKE @strBusca";
+
+
+                cmd.Parameters.AddWithValue("@strBusca", "%" + txtBusca.Text.ToUpper()+ "%");
+                cmd.Prepare();
+
+                /*   --------------------------------------------- */
+                /* versao Antiga antes do statems
+                string strBusca = "'%" + txtBusca.Text.ToUpper() + "%'";
+                //criar Conexao  com Mysql
+                conexao = new MySqlConnection(data_source);
+
+                string sql = "Select * FROM Carro " +
+                    " WHERE " +
+                    " carr_tx_descricao LIKE " + strBusca + " OR carr_tx_placa LIKE " + strBusca;
+
+                */
+
+                                              
+                MySqlDataReader listaCarroSelect = cmd.ExecuteReader();
+
+                lstCarro.Items.Clear();                    
+
+                while(listaCarroSelect.Read())
+                {
+                    string[] linhacarro = {
+                    listaCarroSelect.GetString(0),
+                    listaCarroSelect.GetString(1),
+                    listaCarroSelect.GetString(2),
+                    };
+                    var linha_listview = new ListViewItem(linhacarro);
+
+                     lstCarro.Items.Add(linha_listview);
+                } 
+                 
+
+            }
+            catch (MySqlException Ex)
+            {
+                MessageBox.Show("Erro" + Ex.Number + "Ocorreu : " + Ex.Message, "Erro", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+        }
+
+        
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            limpa_formulario();
+        }
+
+        private void limpa_formulario()
+        {
+            id_Carro_Selecionado = null;
+            txtDescricao.Text = String.Empty;
+            txtPlaca.Text = "";
+            button3.Visible = false;
+            txtDescricao.Focus();
+        }
+
+        private void lstCarro_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            ListView.SelectedListViewItemCollection itens_Selecionados = lstCarro.SelectedItems;
+            foreach (ListViewItem item in itens_Selecionados)
+            {
+                id_Carro_Selecionado = Convert.ToInt32(item.SubItems[0].Text);
+                txtDescricao.Text = item.SubItems[1].Text;
+                txtPlaca.Text = item.SubItems[2].Text;
+                button3.Visible = true;
+            } 
+
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            excluir_Carro();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            //butao excluir
+            excluir_Carro();
+        }
+
+        private void excluir_Carro()
+        {
+            try
+            {
+                DialogResult conf = MessageBox.Show("Confirma a exclusão do Carro",
+                                                    "Exclusao de carro",
+                                                    MessageBoxButtons.YesNo,
+                                                    MessageBoxIcon.Warning);
+                if (conf == DialogResult.Yes)
+                {
+                    conexao = new MySqlConnection(data_source);
+                    conexao.Open();
+
+                    MySqlCommand cmd = new MySqlCommand();
+
+                    cmd.Connection = conexao;
+                    cmd.Parameters.AddWithValue("@id", id_Carro_Selecionado);
+                    cmd.CommandText = "DELETE  FROM Carro WHERE carr_ID_CARRO=@id";
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("CARRO Excluido com sucesso!",
+                                    "Exclusão", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    limpa_formulario();
+                    carrega_Carro();                    
+                    // MessageBox.Show("Preparando para Exclusao Id ->" + id_Carro_Selecionado);
+                }
+
+            }
+            catch (MySqlException Ex)
+            {
+                MessageBox.Show("Erro" + Ex.Number + "Ocorreu : " + Ex.Message, "Erro", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+    }
+}
